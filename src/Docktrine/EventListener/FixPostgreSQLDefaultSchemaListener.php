55ee86a10375144928c94b94249c76a3e397fde9
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-04-23 11:37:55 +0200

    fix: FixPostgreSQLDefaultSchemaListener - create migration w/o schema

---
+++ src/Docktrine/EventListener/FixPostgreSQLDefaultSchemaListener.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine\EventListener;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\PostgreSQLSchemaManager;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+
+final class FixPostgreSQLDefaultSchemaListener
+{
+    /**
+     * @throws SchemaException
+     * @throws Exception
+     */
+    public function postGenerateSchema(GenerateSchemaEventArgs $args): void
+    {
+        $schemaManager = $args
+            ->getEntityManager()
+            ->getConnection()
+            ->createSchemaManager();
+
+        if (!$schemaManager instanceof PostgreSQLSchemaManager) {
+            return;
+        }
+
+        $schema = $args->getSchema();
+
+        foreach ($schemaManager->listSchemaNames() as $namespace) {
+            if (!$schema->hasNamespace($namespace)) {
+                $schema->createNamespace($namespace);
+            }
+        }
+    }
+}
